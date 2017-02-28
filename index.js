7a05f5293c4725d4d0c4a03116fb377d5046304a
Tim Brockman <tim@timbrockman.com>
2017-02-28 02:01:03 -0500

    start extract functions

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
 var striptags = require('striptags');
 var request = require('superagent');
+
+var url = 'https://www.jrcigars.com/item/saint-luis-rey-cigars/saint-luis-rey-reserva-especial/belicoso/sl15b';
+var jrExtract = url => {
+
+};
+var tcExtract = url => {
+
+};
+var fsExtract = url => {
+
+};
+var ciExtract = url => {
+
+};
+var gcExtract = url => {
+
+};
+var btExtract = url => {
+
+};
+
+request.get(url)
+  .end(function(e, response){
+  console.log('is ok?', response.ok);
+  var html = String(response.text);
+  var brand = striptags(html.match(/<h1 itemprop=\"brand\" class=\"tight\">([a-zA-Z0-9 _-])*<\/h1>/)[0]);
+  console.log(brand);
+});
