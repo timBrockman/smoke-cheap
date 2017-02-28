c8bc1765721dcdf84ec2e43528bdbe68af3998ed
Tim Brockman <tim@timbrockman.com>
2017-02-28 02:40:11 -0500

    jr desc

--- index.js
+++ index.js
@@ -25,6 +25,8 @@ request.get(url)
   .end(function(e, response){
   console.log('is ok?', response.ok);
   var html = String(response.text);
-  var brand = striptags(html.match(/<h1 itemprop=\"brand\" class=\"tight\">([a-zA-Z0-9 _-])*<\/h1>/)[0]);
+  var brand = striptags(html.match(/<h1 itemprop=\"brand\" class=\"tight\">([a-zA-Z0-9 _-]*)<\/h1>/)[0]);
+  var description = striptags(html.match(/<div class=\"item-description\">\s*<p>(.*)<\/p><\/div><a href=\"#read-more\"/)[0]);
   console.log(brand);
+  console.log(description);
 });
